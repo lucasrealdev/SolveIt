07ad3cae948f3310fb956795918d252c396726c0
lucasrealdev <Lucas.hpr.06@gmail.com>
2024-11-18 14:59:14 -0300

    feat: novo layout para tela amigos

--- SolveIt-Frontend/src/app/(tabs)/friends.tsx
+++ SolveIt-Frontend/src/app/(tabs)/friends.tsx
@@ -41,59 +41,78 @@ export default function Friends() {
     <ScrollView showsVerticalScrollIndicator={false} className="flex-1">
       <SearchHeader />
       <View className="flex-1 bg-backgroundStandardDark items-center">
-        <View className="max-w-[1000px] w-full bg-backgroundStandardDark px-[10px] py-6 gap-4">
-          <View accessibilityLabel="ContainerSugestaoAmigos">
+        <View className="max-w-[1000px] w-full bg-backgroundStandardDark px-[10px] py-6 gap-7">
 
-            <View className="w-full flex flex-row justify-between mb-6 items-center">
+          <View className="gap-3" aria-label="ContainerPerfil">
+            <View className="w-full flex-row justify-between items-center">
+              <Text className="font-bold text-xl text-textStandardDark text-nowrap">
+                Seu Perfil
+              </Text>
+
+              <View className="flex-row gap-2 flex-1 items-center justify-end">
+                <View aria-label="Buttons" className="w-full h-[45px] rounded-[20px] flex-row items-center justify-between max-w-[311px] bg-textStandardDark">
+                  <Animated.View style={[styles.activeBar, { transform: [{ translateX }], width: buttonWidth }]} />
+
+                  <Pressable className="flex-1 items-center justify-center bg-transparent" onPress={moveToSeguidores} onLayout={handleButtonLayout}>
+                    <Text className="text-white text-[14px] font-bold">Seguidores</Text>
+                  </Pressable>
+
+                  <Pressable className="flex-1 items-center justify-center bg-transparent" onPress={moveToSeguindo} onLayout={handleButtonLayout}>
+                    <Text className="text-white text-[14px] font-bold">Seguindo</Text>
+                  </Pressable>
+                </View>
+                <ButtonScale
+                  scale={1.07}
+                  className="flex p-[11px] border border-borderStandard rounded-full">
+                  <CustomIcons name="chat" size={24} color="#475569" />
+                </ButtonScale>
+              </View>
+            </View>
+
+            <View aria-label="ContainerSeguidores" className="rounded-[10px] border border-borderStandardLight bg-white">
+              <CardAmigo label="amigo" />
+              <CardAmigo label="amigo" />
+              <CardAmigo label="amigo" />
+              <CardAmigo label="amigo" />
+              <CardAmigo label="amigo" />
+              <HoverColorComponent className="flex flex-row gap-2 w-full justify-center my-4" colorHover={colors.accentStandardDark.hover}
+                colorPressIn={colors.accentStandardDark.pressIn}>
+                <Text className="font-bold text-[15px] justify-center" style={{ color: "#01b297" }}>
+                  Carregar Mais
+                </Text>
+
+                <CustomIcons name="mais"
+                  color='#01B198'
+                  size={20} />
+              </HoverColorComponent>
+            </View>
+          </View>
+
+
+          <View aria-label="ContainerSugestaoAmigos" className="gap-3">
+            <View className="w-full flex flex-row justify-between items-center">
               <Text className="font-bold text-xl text-textStandardDark">
                 Sugestão de amigos
               </Text>
-              <ButtonScale
-                scale={1.07}
-                className="flex p-[11px] border border-borderStandard rounded-full">
-                <CustomIcons name="chat" size={24} color="#475569" />
-              </ButtonScale>
             </View>
 
-            <View accessibilityLabel="ContainerAmigos" className="rounded-[10px] border border-borderStandardLight bg-white">
+            <View aria-label="ContainerAmigos" className="rounded-[10px] border border-borderStandardLight bg-white">
               <CardAmigo label="seguidores" />
               <CardAmigo label="seguidores" />
               <CardAmigo label="seguidores" />
               <CardAmigo label="seguidores" />
               <CardAmigo label="seguidores" />
+              <HoverColorComponent className="flex flex-row gap-2 w-full justify-center my-4" colorHover={colors.accentStandardDark.hover}
+                colorPressIn={colors.accentStandardDark.pressIn}>
+                <Text className="font-bold text-[15px] justify-center" style={{ color: "#01b297" }}>
+                  Carregar Mais
+                </Text>
+
+                <CustomIcons name="mais"
+                  color='#01B198'
+                  size={20} />
+              </HoverColorComponent>
             </View>
-            <HoverColorComponent className="flex flex-row items-end gap-2 w-fit mt-4" colorHover={colors.accentStandardDark.hover}
-              colorPressIn={colors.accentStandardDark.pressIn}>
-              <Text className={`font-bold text-[15px]`} style={{color: "#01b297"}}>
-                Carregar Mais
-              </Text>
-
-              <CustomIcons name="mais"
-                color='#01B198'
-                size={20} />
-            </HoverColorComponent>
-          </View>
-
-          <View accessibilityLabel="Linha" className="w-full bg-borderStandardLight h-[1px] rounded-xl"></View>
-
-          <View accessibilityLabel="Buttons" className="w-full h-[45px] rounded-[20px] flex-row items-center justify-between max-w-[311px] bg-textStandardDark">
-            <Animated.View style={[styles.activeBar, { transform: [{ translateX }], width: buttonWidth }]} />
-
-            <Pressable className="flex-1 items-center justify-center bg-transparent" onPress={moveToSeguidores} onLayout={handleButtonLayout}>
-              <Text className="text-white text-[14px] font-bold">Seguidores</Text>
-            </Pressable>
-
-            <Pressable className="flex-1 items-center justify-center bg-transparent" onPress={moveToSeguindo} onLayout={handleButtonLayout}>
-              <Text className="text-white text-[14px] font-bold">Seguindo</Text>
-            </Pressable>
-          </View>
-
-          <View accessibilityLabel="ContainerSeguidores" className="rounded-[10px] border border-borderStandardLight bg-white">
-            <CardAmigo label="amigo" />
-            <CardAmigo label="amigo" />
-            <CardAmigo label="amigo" />
-            <CardAmigo label="amigo" />
-            <CardAmigo label="amigo" />
           </View>
         </View>
       </View>
